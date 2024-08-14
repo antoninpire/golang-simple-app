56000d763988bcd98c32f40563f7091eb2759c82
antoninpire <antonin.pire@gmail.com>
2024-08-15 03:13:50 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-simple-app
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Example struct {
+	ID   string `json:"id"`
+	Data int    `json:"data"`
+}
+
+func main() {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		data := Example{
+			ID:   "some_random_id",
+			Data: 100,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
+	})
+	log.Fatal(http.ListenAndServe(":3333", nil))
+}
